3678e2b050383746f4d52269660b9279b344e82e
Princinho <princegnakou@gmail.com>
2022-11-02 11:26:55 +0000

    add navigation

---
+++ scripts.js
@@ -0,0 +1,25 @@
+document.querySelectorAll('a[href^="#"]').forEach(a => {
+    a.addEventListener('click', function (e) {
+        e.preventDefault();
+        var href = this.getAttribute("href");
+        console.log(href)
+        var elem = document.querySelector(href)||document.querySelector("a[id="+href.substring(1, href.length)+"]");
+        //gets Element with an id of the link's href 
+        //or an anchor tag with a name attribute of the href of the link without the #
+        window.scroll({
+            top: elem.offsetTop, 
+            left: 0, 
+            behavior: 'smooth' 
+        });
+        //if you want to add the hash to window.location.hash
+        //you will need to use setTimeout to prevent losing the smooth scrolling behavior
+       //the following code will work for that purpose
+       /*setTimeout(function(){
+            window.location.hash = this.hash;
+        }, 2000); */
+        document.body.classList.toggle('nav-shown')
+    });
+});
+function showNav(){
+    document.body.classList.toggle('nav-shown')
+}
